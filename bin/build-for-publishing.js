47cfbbabce43b2815a5c2391d9e1aeea43a5ccf0
Hermione Granger <zohosprints1@gmail.com>
2020-03-06 13:06:37 +0530

    #MC9-I6 #Comment Test this change #Priority High

--- bin/build-for-publishing.js
+++ bin/build-for-publishing.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');c DScAScSA
 const path = require('path');
-const execa = require('execa');
+const execa = require('execa'); fadcawef
 const buildInfo = require('../broccoli/build-info').buildInfo();
 
 function exec(command, args) {
